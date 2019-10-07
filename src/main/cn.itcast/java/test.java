64772562c4d7a9bd51ff78e4dee2bfdb6c243124
txmore <18292000734@163.com>
2019-10-07 22:56:42 +0800

    添加此工程到git

---
+++ src/main/cn.itcast/java/test.java
@@ -0,0 +1,10 @@
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("111111111111");
+        System.out.println("122222222");
+        System.out.println("113333333");
+        System.out.println("1117787878781");
+        System.out.println("11154545451111");
+    }
+}
